8b655a089c61dc16f2f54f55cc06a8f3317ed342
Tyler Etters <tyler@etters.co>
2025-06-14 22:21:13 -0700

    ++

--- dist/index.es.js
+++ dist/index.es.js
@@ -1,6 +1,6 @@
 const e = [
   {
-    title: "midwest_sad, part 2",
+    title: "midwest sad, part 2",
     project: "DJ STUXNET",
     released: "02025-06-14",
     type: "Mix",
@@ -12,36 +12,36 @@ const e = [
     tracks: [
       {
         number: 1,
-        title: "midwest_sad, part 2",
+        title: "midwest sad, part 2",
         length: "00:59:25",
-        mp3_url: "https://d107e1o0dn11sc.cloudfront.net/dj-stuxnet/midwestsad-part-2/01-midwestsad-part-2.mp3",
-        wav_url: "https://d107e1o0dn11sc.cloudfront.net/dj-stuxnet/midwestsad-part-2/01-midwestsad-part-2.wav",
-        id: "923a29b1e326f3cd49589cf2a359ad8c"
+        mp3_url: "https://d107e1o0dn11sc.cloudfront.net/dj-stuxnet/midwest-sad-part-2/01-midwest-sad-part-2.mp3",
+        wav_url: "https://d107e1o0dn11sc.cloudfront.net/dj-stuxnet/midwest-sad-part-2/01-midwest-sad-part-2.wav",
+        id: "7c9c502e4aff49266e56b6e2b7086cd3"
       }
     ],
     streams: [
       {
         platform: "SoundCloud",
         url: "https://soundcloud.com/tyleretters/midwest-sad-part-2",
-        id: "c2994374ee9c1a86c0bd620cf01e86b6"
+        id: "e3be3101eb05432f74e708e5fa736f43"
       },
       {
         platform: "Untitled Stream",
         url: "https://untitled.stream/library/project/I1BQS4lOiZu2EUiKqXRPQ",
-        id: "6fdb7bb917462e0a7e7c53fb2aa1b1b1"
+        id: "4b5556fe09aab796f764609d54a8b50b"
       }
     ],
     notes: "<p>Woah-oh-oh-oh, oh-oh-oh, oh-oh-oh.</p><p>Woah-oh-oh-oh, oh-oh-oh, oh-oh-oh.</p><p>Woah-oh-oh-oh, oh-oh-oh, oh-oh-oh.</p><p>Woah-oh-oh-oh, oh-oh-oh, oh-oh-oh.</p><p>Now, we're coming home.</p>",
     credits: "<p>STUXNET is Tyler Etters.</p>",
     project_slug: "dj-stuxnet",
-    release_slug: "midwestsad-part-2",
-    cover_url: "https://d107e1o0dn11sc.cloudfront.net/dj-stuxnet/midwestsad-part-2/midwestsad-part-2.jpg",
-    mp3_url: "https://d107e1o0dn11sc.cloudfront.net/dj-stuxnet/midwestsad-part-2/midwestsad-part-2-mp3.zip",
-    wav_url: "https://d107e1o0dn11sc.cloudfront.net/dj-stuxnet/midwestsad-part-2/midwestsad-part-2-wav.zip",
-    id: "4fc7ab2679a9ad08b467b9a440393dda"
+    release_slug: "midwest-sad-part-2",
+    cover_url: "https://d107e1o0dn11sc.cloudfront.net/dj-stuxnet/midwest-sad-part-2/midwest-sad-part-2.jpg",
+    mp3_url: "https://d107e1o0dn11sc.cloudfront.net/dj-stuxnet/midwest-sad-part-2/midwest-sad-part-2-mp3.zip",
+    wav_url: "https://d107e1o0dn11sc.cloudfront.net/dj-stuxnet/midwest-sad-part-2/midwest-sad-part-2-wav.zip",
+    id: "e5475132c4f7e2e537edb2e2c94f48a3"
   },
   {
-    title: "midwest_sad, part 1",
+    title: "midwest sad, part 1",
     project: "DJ STUXNET",
     released: "02025-06-14",
     type: "Mix",
@@ -53,33 +53,33 @@ const e = [
     tracks: [
       {
         number: 1,
-        title: "midwest_sad, part 1",
+        title: "midwest sad, part 1",
         length: "00:59:18",
-        mp3_url: "https://d107e1o0dn11sc.cloudfront.net/dj-stuxnet/midwestsad-part-1/01-midwestsad-part-1.mp3",
-        wav_url: "https://d107e1o0dn11sc.cloudfront.net/dj-stuxnet/midwestsad-part-1/01-midwestsad-part-1.wav",
-        id: "192eb77b0c6277b9119b88d2ae48edd2"
+        mp3_url: "https://d107e1o0dn11sc.cloudfront.net/dj-stuxnet/midwest-sad-part-1/01-midwest-sad-part-1.mp3",
+        wav_url: "https://d107e1o0dn11sc.cloudfront.net/dj-stuxnet/midwest-sad-part-1/01-midwest-sad-part-1.wav",
+        id: "5b5d2a4ba6d6ed1cadccb3ac93e55abc"
       }
     ],
     streams: [
       {
         platform: "SoundCloud",
         url: "https://soundcloud.com/tyleretters/midwest-sad-part-1",
-        id: "6896b2c1ab7dda389468f320f1bf4118"
+        id: "bd4362f394639f4512929542909193db"
       },
       {
         platform: "Untitled Stream",
         url: "https://untitled.stream/library/project/lYBChFUYCuureLWbUKfye",
-        id: "54b0a90b7df6de62126bbdb962c04b8d"
+        id: "86c2369981491c094d7e21915bd8a96a"
       }
     ],
-    notes: `<p>If "you had to be there" then I wouldn't have made this mixtape. It was a very specific time and place. But the music we played was just "music" to me. It would be years later until I learned that it was all actually emo, midwest-emo, screamo, hardcore, metalcore, progressive... It was folk music for us, right? So we start in the midwest. Appleseed Cast (Kansas) then over to Dr. Manhattan (Illinois), The Felix Culpa (also Illinois), and then a bit less underground when we climb the Seer's Tower with Mr. Stevens. It goes on from there. This is a time capsule. This is midwest_sad, part 1. Man your own jackhammer.</p>`,
+    notes: `<p>If "you had to be there" then I wouldn't have made this mixtape. It was a very specific time and place. But the music we played was just "music" to me. It would be years later until I learned that it was all actually emo, midwest-emo, screamo, hardcore, metalcore, progressive... It was folk music for us, right? So we start in the midwest. Appleseed Cast (Kansas) then over to Dr. Manhattan (Illinois), The Felix Culpa (also Illinois), and then a bit less underground when we climb the Seer's Tower with Mr. Stevens. It goes on from there. This is a time capsule. This is midwest sad, part 1. Man your own jackhammer.</p>`,
     credits: "<p>STUXNET is Tyler Etters.</p>",
     project_slug: "dj-stuxnet",
-    release_slug: "midwestsad-part-1",
-    cover_url: "https://d107e1o0dn11sc.cloudfront.net/dj-stuxnet/midwestsad-part-1/midwestsad-part-1.jpg",
-    mp3_url: "https://d107e1o0dn11sc.cloudfront.net/dj-stuxnet/midwestsad-part-1/midwestsad-part-1-mp3.zip",
-    wav_url: "https://d107e1o0dn11sc.cloudfront.net/dj-stuxnet/midwestsad-part-1/midwestsad-part-1-wav.zip",
-    id: "f2ea2edfac50b70916a9e58cb68f7616"
+    release_slug: "midwest-sad-part-1",
+    cover_url: "https://d107e1o0dn11sc.cloudfront.net/dj-stuxnet/midwest-sad-part-1/midwest-sad-part-1.jpg",
+    mp3_url: "https://d107e1o0dn11sc.cloudfront.net/dj-stuxnet/midwest-sad-part-1/midwest-sad-part-1-mp3.zip",
+    wav_url: "https://d107e1o0dn11sc.cloudfront.net/dj-stuxnet/midwest-sad-part-1/midwest-sad-part-1-wav.zip",
+    id: "4727ddb5945626312dac4e30a3ae5b45"
   },
   {
     title: "Brooklyn Vengeance",
